e3c58f2417730046280bcca901206533e532ca6e
Trevor Watson <twatson603@gmail.com>
2016-01-28 13:21:27 -0500

    add initial files

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+# from app import views
+
+
+# urlpatterns = patterns('',
+#     url(r'^$', views.index_view, name='index'),
+#     url(r'^another/$', views.another_view, name='another'),
+# )
