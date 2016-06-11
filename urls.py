4543bcd784c5fde15f26ec25a4c05fc33e18ec65
Trevor Watson <trevor@trevorwatson.me>
2016-06-11 14:14:19 -0400

    update urlpatterns to conform to changes in Django 1.9

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 # from app import views
 
 
-# urlpatterns = patterns('',
-#     url(r'^$', views.index_view, name='index'),
-#     url(r'^another/$', views.another_view, name='another'),
-# )
+urlpatterns = [
+    # url(r'^$', views.index_view, name='index'),
+    # url(r'^another/$', views.another_view, name='another'),
+]
